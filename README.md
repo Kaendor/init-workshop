246c0e7e9cea86f14dca926f5288b005d28101ad
Dimitri Teboul <dimitri.teboul@indy.fr>
2024-03-26 17:50:16 +0100

    add first activity

--- README.md
+++ README.md
@@ -6,16 +6,23 @@
 - Pouvoir comparer simplement ses features avec d'autres langages
 - Avoir une API web simple
 
+## Déroulé
+
+- Séparation en plusieurs groupes de taille max de 3 en mode Mob/Pair
+- Découverte d'une feature de Rust
+- Regroupement ?
+
 ## Etapes
 
-- Variables
+- Variables & Control flow
+- Modules
 - Structs
+- Iterators
 - Enum
 - Pattern matching
 - Ownership
 - Tests
 - Docs
-- Modules
 - Traits
 - Async
 
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,39 @@
+mod enums;
+mod iterators;
+mod structs;
+
 fn main() {
-    println!("Hello, world!");
+    variables_and_control_flow();
+}
+
+fn variables_and_control_flow() {
+    // Les variables en Rust sont par défaut immutables
+    let url = "https://indy.fr";
+
+    // Rust favorise les approches immutable mais permet de muter au besoin avec le mot clé mut
+    let counter = 0;
+    // On peut voir que le compilateur indique clairement qu'on ne peut pas muter une variable
+    // immutable.
+
+    // Il propose une solution si on survol l'erreur avec le LSP.
+    // A l'inverse de beaucoup de langage, un effort est fait sur les erreurs de compilation afin
+    // qu'elles aident plus qu'elles ne paralysent.
+    counter += 1;
+
+    // Pas besoin de parenthèse autour de la condition en Rust
+    if counter >= 0 {
+        println!("Je suis l'équivalent d'un console.log() (mais en mieux), parce que j'ai de l'interpolation {url}");
+    }
+
+    // Une particularité de Rust qui peut être un peu étrange au début, c'est que beaucoup
+    // d'expressions peuvent être attribuées à une variable.
+    // Par exemple
+    let iteration_count = if counter == 1 { 30 } else { 0 };
+
+    // Les boucles en rust sont similaire à celles qu'on retrouve dans d'autre langage
+    // A la difference près qu'elles prennent des Iterators en entrée
+    // Ici on voit un Range, qui est un Iterator sur des chiffres entiers.
+    for c in 0..iteration_count {
+        println!("{c}");
+    }
 }
